54e1f730e3c16f9d37bd029d9bce74c3e15f28bc
Nathan Fleet <nfleet@kent.edu>
2024-11-12 17:09:08 -0500

    Inital Xcode commit

---
+++ ComicKeep/ComicKeepTests/ComicKeepTests.swift
@@ -0,0 +1,17 @@
+//
+//  ComicKeepTests.swift
+//  ComicKeepTests
+//
+//  Created by Nathan Fleet on 11/12/24.
+//
+
+import Testing
+@testable import ComicKeep
+
+struct ComicKeepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
